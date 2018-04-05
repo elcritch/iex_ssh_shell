e50f37c214d192b1fb6f6a9234c116b1e9ec7029
Jaremy Creechley <creechley@gmail.com>
2018-04-05 12:38:49 -0600

    Adding basic documentation

--- lib/nerves_ssh_shell.ex
+++ lib/nerves_ssh_shell.ex
@@ -3,6 +3,28 @@ defmodule NervesSshShell do
   NervesSshShell provides a simple OTP application which runs an SSH server for remote IEx shells.
 
   This is useful to avoid the pitfalls with using disterl for many embedded device scenarios.
+
+  NervesSshShell provides a simple OTP application which runs an SSH server for remote IEx shells.
+
+  This is useful to avoid the pitfalls with using disterl for many embedded device scenarios.
+
+  Configuration is handled by settings `:nerves_ssh_shell` configuration options in your project's config.exs.
+
+  Example:
+
+  ```elixir
+  config :nerves_ssh_shell,
+  system_dir: "/etc/ssh",
+  port: 2222,
+  authorized_keys: [
+  "ssh-rsa ... ",
+  "ssh-rsa ... ",
+  ...
+  ]
+
+  ```
+
+
   """
 
 end
