0c4949260e77abb7f7181b8cea4c5e376dd833b1
Jaremy Creechley <creechley@gmail.com>
2018-10-31 20:11:33 -0600

    testing

--- lib/nerves_ssh_shell/iex_daemon.ex
+++ lib/nerves_ssh_shell/iex_daemon.ex
@@ -35,6 +35,7 @@ defmodule IExSshShell.IEx.Daemon do
       {:user_dir, sys_dir },
       {:auth_methods, 'publickey' },
       {:shell, {Elixir.IEx, :start, []}},
+      {:shell, {Application.fetch_env!(:iex_ssh_shell, :handler), :start, []}},
       {:failfun, &on_shell_unauthorized/3},
       {:connectfun, &on_shell_connect/3},
       # {:shell, &do_shell/2},
